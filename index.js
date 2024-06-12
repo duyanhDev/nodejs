4933243adba9a82db34af0b6d69af21dc4122343
duyanhDev <loveeasy20@gmail.com>
2024-06-12 23:36:16 +0700

    brach 2

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// cập nhật file env
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+
 // config template enginees
 app.set("views", path.join(__dirname, "./src/views"));
-
 app.set("view engine", "ejs");
 
+// config files  public
+app.use(express.static(path.join(__dirname, "./src/public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World\n duy anh");
+  res.send("Hello World\n duy anh dep trai 134");
 });
 app.get("/abc", (req, res) => {
   res.send("check abc");
@@ -16,4 +23,4 @@ app.get("/abc", (req, res) => {
 app.get("/hoidanit", (req, res) => {
   res.render("sample.ejs");
 });
-app.listen(8000);
+app.listen(port);
